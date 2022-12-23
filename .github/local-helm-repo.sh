9ef4120695486cb7d1c78a6d2ddea801c8116bbf
Marco Franssen <marco.franssen@philips.com>
2022-12-23 14:49:40 +0100

    Use setup-chartmuseum in workflow to install chartmuseum
    Signed-off-by: Marco Franssen <marco.franssen@philips.com>

--- .github/local-helm-repo.sh
+++ .github/local-helm-repo.sh
@@ -2,18 +2,6 @@
 
 set -euo pipefail
 
-SCRIPTPATH=$(dirname "$0")
-
-function install_chartmuseum {
-  curl https://raw.githubusercontent.com/helm/chartmuseum/main/scripts/get-chartmuseum | bash
-}
-
-if [ ! -f "/usr/local/bin/chartmuseum" ] ; then
-  install_chartmuseum
-else
-  echo "Using '$(/usr/bin/chartmuseum --version)'"
-fi
-
 chartmuseum --debug --port=8879 \
   --storage="local" \
   --storage-local-rootdir="./chartmuseum" &
@@ -22,9 +10,9 @@ sleep 1
 
 helm repo add philips-labs http://127.0.0.1:8879
 
-for chart in $(ls -d charts/* | grep -v dctna); do
+for chart in $(find charts -maxdepth 1 -type d | grep -v dctna); do
   helm package -u "$chart"
-  chart_package=$(ls -f | grep "${chart#*/}")
+  chart_package=$(find charts -maxdepth 1 -type f | grep "${chart}")
   curl --data-binary "@$chart_package" http://localhost:8879/api/charts
   rm -rf "$chart_package"
 done
